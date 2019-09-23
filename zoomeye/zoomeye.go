b20998c4b8ff543e7c2e4a7c28b525f3ec5b5ef1
小葵 <xi4okui@gmail.com>
2019-09-23 18:01:34 +0800

    Update zoomeye.go

--- zoomeye/zoomeye.go
+++ zoomeye/zoomeye.go
@@ -22,71 +22,70 @@ var userName string
 var passWord string
 
 type Auth struct {
-    AccessToken  string `json:"access_token"`
+	AccessToken  string `json:"access_token"`
 }
 
 
 type Portinfo struct {
-	  Port     int   `json:"port"`
+	Port     int   `json:"port"`
 }
 
 type Matche struct {
-	  Ip       string     `json:"ip"`
-		Portinfo Portinfo `json:"portinfo"`
+	Ip       string     `json:"ip"`
+	Portinfo Portinfo `json:"portinfo"`
 }
 
 type Matches struct {
-    Matches  []Matche `json:"matches"`
-		Total		 int      `json:"total"`
+	Matches  []Matche `json:"matches"`
+	Total	 int      `json:"total"`
 }
 
 
 
 func Get(urlStr string) []byte {
-  request, err := http.NewRequest("GET", urlStr, nil)
-  request.Header.Set("Authorization", "JWT " + AccessToken)
-  client := http.Client{}
-  resp, err := client.Do(request)
-  if err != nil {
-      fmt.Println(err.Error())
-  }
-  respBytes, err := ioutil.ReadAll(resp.Body)
-  if err != nil {
-      fmt.Println(err.Error())
-  }
-  return respBytes
+	request, err := http.NewRequest("GET", urlStr, nil)
+	request.Header.Set("Authorization", "JWT " + AccessToken)
+	client := http.Client{}
+	resp, err := client.Do(request)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	respBytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	return respBytes
 }
 
 func Post(urlStr string, data interface{}) []byte {
-  bytesData, err := json.Marshal(data)
-  body := bytes.NewReader(bytesData)
-  request, err := http.NewRequest("POST", urlStr, body)
-  request.Header.Set("Content-Type", "application/json;charset=UTF-8")
-  client := http.Client{}
-  resp, err := client.Do(request)
-  if err != nil {
-      fmt.Println(err.Error())
-  }
-  respBytes, err := ioutil.ReadAll(resp.Body)
-  if err != nil {
-      fmt.Println(err.Error())
-  }
-  return respBytes
+	bytesData, err := json.Marshal(data)
+	body := bytes.NewReader(bytesData)
+	request, err := http.NewRequest("POST", urlStr, body)
+	request.Header.Set("Content-Type", "application/json;charset=UTF-8")
+	client := http.Client{}
+	resp, err := client.Do(request)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	respBytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	return respBytes
 }
 
 func GetAccessToken() string{
-  data := make(map[string]interface{})
-  data["username"] = userName
-  data["password"] = passWord
+	data := make(map[string]interface{})
+	data["username"] = userName
+	data["password"] = passWord
 
-  auth := Auth{}
+	auth := Auth{}
 
-  err :=  json.Unmarshal(Post(defaultBaseURL + getAccessTokenURL,data),&auth)
-  if err!=nil{
-      fmt.Println(err)
-  }
-  return auth.AccessToken
-
+	err :=  json.Unmarshal(Post(defaultBaseURL + getAccessTokenURL,data),&auth)
+	if err!=nil{
+		fmt.Println(err)
+	}
+	return auth.AccessToken
 }
 
 func Search(query string,page int,username string,password string) []string {
